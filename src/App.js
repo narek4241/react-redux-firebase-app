4e66577c815f3081d26e7b4936331b8f803b3305
narek4241 <narekghazaryan02@gmail.com>
2021-02-07 00:14:39 +0400

    setup Navbar comp, and its child comps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Starter</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
