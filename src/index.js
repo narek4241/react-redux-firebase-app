7eac90c3bf7d40f872e509e21e1934e98fa7b28c
narek4241 <narekghazaryan02@gmail.com>
2021-02-08 16:22:33 +0400

    connected redux to firebase

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import {
+  reactReduxFirebase, // removed from API, doc opt
+  ReactReduxFirebaseProvider, // new API, doc opt
+  getFirebase,
+} from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import fbConfig from './config/fbConfig';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    // reactReduxFirebase(fbConfig), // ok?rm
+    reduxFirestore(fbConfig)
+  )
 );
 
 ReactDOM.render(
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // some async statement
     dispatch({
       type: 'CREATE_PROJECT',
