61035421c48f5c36f72a0d6d1a6cb5c7032930ad
narek4241 <narekghazaryan02@gmail.com>
2021-02-08 20:41:28 +0400

    syncing data with firestore <-> removed projects dummy data, left 1 warning(package-forcing)(Warning: Using UNSAFE_componentWillReceiveProps in strict mode is not recommended and may indicate bugs in your code.)

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,9 +1,5 @@
 const initState = {
-  projects: [
-    { id: '1', title: 'help me find peach', content: 'blah blah blah' },
-    { id: '2', title: 'collect all the stars', content: 'blah blah blah' },
-    { id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah' },
-  ],
+  projects: [],
 };
 
 const projectReducer = (state = initState, action) => {
